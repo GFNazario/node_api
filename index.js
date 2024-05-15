cb7ab8846cc03643d0aa51b3e970a3a4cf5ff320
Gabriel Nazario <gnazario@hotmail.com>
2024-05-15 10:55:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+app.use(express.json())
+const port = 3000
+
+const Film = mongoose.model("Film", {
+    title: String,
+    description: String,
+    image_url: String,
+    trailer: String
+})
+
+app.get("/", async (req, res)=>{
+    const films = await Film.find()
+    return res.send(films)
+})
+
+app.delete("/:id", async (req, res) =>{
+    const film = await Film.findByIdAndDelete(req.params.id)
+    return res.send(film)
+})
+
+app.put("/:id", async (req, res) =>{
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer: req.body.trailer
+    })
+    return res.send(film)
+})
+
+app.post("/", async (req, res) =>{
+    const film = new Film({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer: req.body.trailer
+    })
+    await film.save()
+    return res.send(film)
+})
+
+app.listen(port, () =>{
+    mongoose.connect(`mongodb+srv://gabrielnazario:Ga16naza@nazario.fwth1va.mongodb.net/?retryWrites=true&w=majority&appName=Nazario`)
+    console.log("App running");
+})
